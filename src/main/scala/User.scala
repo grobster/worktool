3c3a3feab34af46481f0a5af8f317c84b614c930
grobster <jquarles@grobster.com>
2017-07-23 16:10:37 -0400

    PhoneNumber object created.

--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -7,8 +7,15 @@ case class Asset(make: String, model: String, serialNumber: String, assetNumber:
 case class Request(number: Long, user: User, asset: Asset, problem: String, notes: Option[String])
 
 object User {
-	def findByLastName(lastName: String, users: List[User]): List[User] = users.filter(u => u.lastName.toLowerCase == lastName)
+	def findByLastName(lastName: String, users: List[User]): List[User] = users.filter(u => u.lastName.toLowerCase == lastName.toLowerCase)
 	
-	def findByFirstName(firstName: String, users: List[User]): List[User] = users.filter(u => u.firstName.toLowerCase == firstName)
+	def findByFirstName(firstName: String, users: List[User]): List[User] = users.filter(u => u.firstName.toLowerCase == firstName.toLowerCase)
+	
+	def findByEmailAddress(emailAddress: String, users: List[User]): Option[User] = users.filter(u => u.emailAddress.toLowerCase == emailAddress.toLowerCase).headOption
+
+}
+
+object PhoneNumber {
+	def parsePhoneNumber(text: String): String = text.toArray.filter(c => Character.isDigit(c)).foldRight("")(_ + _)
 
 }
