ec58851f768e0a709ba90355d8f18d2f212d05fb
grobster <jquarles@grobster.com>
2017-07-24 21:33:15 -0400

    made changes

--- src/main/scala/UserView.scala
+++ src/main/scala/UserView.scala
@@ -6,15 +6,26 @@ import scalafx.scene.Scene
 import scalafx.scene.control.TextField
 import scalafx.scene.control.Button
 import scalafx.scene.control.MenuBar
+import scalafx.scene.control.Menu
+import scalafx.scene.control.MenuItem
 
 object UserView extends JFXApp {
 	stage = new PrimaryStage {
 		title = "User Tool"
 		
 		scene = new Scene {
+			val menuBar = new MenuBar
+			val fileMenu = new Menu("File")
+			val menuItem = new MenuItem("Open")
+			fileMenu.items = List(menuItem)
+			
+			menuBar.menus = List(fileMenu)
+			
 			content = Seq(
 				new TextField {
 					text = "Test"
+					layoutX = 200
+					layoutY = 200
 					
 				}, 
 				new Button{
@@ -23,10 +34,7 @@ object UserView extends JFXApp {
 					layoutY = 200
 				},
 				
-				new MenuBar{
-				
-				
-				}
+				menuBar
 			)
 		}
 	
